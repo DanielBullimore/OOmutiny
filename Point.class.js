ab0c0d2a9a934d13670830a65017f95a4b52a9ea
Daniel <50190195+DanielBullimore@users.noreply.github.com>
2020-01-07 23:40:08 +1300

    Waiting for the inbound  new plymouth truck

---
+++ Point.class.js
@@ -0,0 +1,62 @@
+//Javascript Document
+//* File Name: OoPoint.class.js
+//* System: OOmutiny
+//* Dependencies: OO.class.js
+//****************************************************************************************************** 	//* Author:           <Your name>
+//* Authors email: daniel.k.bullimore@gmail.com
+//* Written:          07/01/20
+//******************************************************************************************************
+//* !---If you modify this file, add your name to this then update the Last Modified date---!
+//* Modifications By: <name1>, <name2>, … , <Your Name>
+//* Last Modified:      <Date dd/mm/yyyy>
+//******************************************************************************************************
+//*
+//*
+//******************************************************************************************************
+//* DESCRIPTION
+//******************************************************************************************************
+/*
+
+An x,y,z axis triangulation point.
+
+Properties:
+ numX - private number, x axis index value.
+ numY - private number, y axis index value.
+ numZ - private number, z axis index value.
+
+Methods:
+ funSetPoint() - takes 3 numbers as the x y z index to define the point.
+ funGetPoint() - returns an array containing the three indexes of the point
+ funGetAxisX() - returns the index value of axis X.
+ funGetAxisY() - returns the index value of axis Y.
+ funGetAxisZ() - returns the index value of axis Z.
+*/
+// [Describe any other usefull stuff]
+//
+//****************************************************************************************************** 
+function OoPoint() 
+{
+//###############
+//# PROPERTIES #
+//###############
+
+//###################
+//# DERIVED VALUES #
+//###################
+	
+
+//#########################
+//# CONSTRUCT DESTRUCT #
+//#########################
+	/*
+	Description: 
+
+	Parameters: 
+              
+
+	Returns:
+	*/
+//##############
+//# FUNCTIONS #
+//##############
+}
