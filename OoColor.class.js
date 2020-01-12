56fd99bc21689cfb2e673b70fc65fc649e770a25
DanielBullimore <daniel.k.bullimore@gmail.com>
2020-01-12 13:43:27 +1300

    Initial Color Branching

---
+++ OoColor.class.js
@@ -0,0 +1,29 @@
+//Javascript Document
+//Copyright(c) 2020, Daniel Bullimore
+//All rights reserved.
+//* File Name: OoColor.class.js
+//* System: OOmutiny
+//* Dependencies: OO.class.js
+//****************************************************************************************************** 	
+//* Author:        Daniel Bullimore
+//* Authors email: daniel.k.bullimore@gmail.com
+//* Written:       12/01/20
+//******************************************************************************************************
+//* !---If you modify this file, add your name to this then update the Last Modified date---!
+//* Modifications By: <name1>, <name2>, … , <Your Name>
+//* Last Modified:      <Date dd/mm/yyyy>
+//******************************************************************************************************
+//*
+//*
+//******************************************************************************************************
+//* DESCRIPTION
+//******************************************************************************************************
+/*
+
+Designed for code reuse. Because many classes will require color as a property and each class would have
+get, set, and validation code. With a single color class any object that requires a color property can
+accept an instance of OoColor as that color.
+
+# OOmutiny is licensed under the BSD 3 - Clause "New" or "Revised" License
+# To view a copy of this license, visit https://github.com/DanielBullimore/OOmutiny/blob/master/LICENSE
+*******************************************************************************************************/
